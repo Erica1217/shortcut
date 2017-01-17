63b2b2359fdbcb5d28dd690fa5eaaf85cfa0c421
Erica1217 <ericakim@sowrender.com>
2017-01-17 22:56:45 +0900

    Change all file
    This is incomplete.

--- Shortcut-Erica/Program.cs
+++ Shortcut-Erica/Program.cs
@@ -10,6 +10,30 @@ namespace Shortcut_Erica
     {
         static void Main(string[] args)
         {
+            string input;
+            string[] inputCut = new string[2];
+            int n, d;
+            int[][] data= new int[10010][];
+            input = Console.ReadLine();
+            inputCut = input.Split(' ');
+            n = int.Parse(inputCut[0]);
+            d = int.Parse(inputCut[1]);
+
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                int[] tmp = new int[] { int.Parse(strArr[0]), int.Parse(strArr[1]), int.Parse(strArr[2]) };
+                for(int j=0; j<3; j++)
+                {
+                    Console.WriteLine(int.Parse(strArr[j]) + " ");
+                    data[i] = tmp;
+                }
+           
+            }
+
+            Console.Write(Shortcut.shortcut(n,d,data).ToString());
+            Console.ReadKey();
         }
     }
 }
---
+++ Shortcut-Erica/Shortcut.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortcut_Erica
+{
+    public class Shortcut
+    {
+        public static int shortcut(int N, int D, int[][] array)
+        {
+            int[] data = new int[10010];
+            for(int i=0; i<=D; i++)
+            {
+                data[i] = i;
+                Console.WriteLine(data[i] + "");
+            }
+
+            // array배열을 0번째 인덱스를 기준으로 정렬
+            twoDimensionalArraySort(0,N, array);
+
+            for (int i = 0; i < N; i++)
+            {
+                int start = array[i][0];
+                int end = array[i][1];
+                int distance = array[i][2];
+
+                if(data[end] > distance)
+                {
+                    for (int j = end; j <= D ; j++)
+                    {
+                        data[j] = distance + (j - end);
+                    }
+                }
+            }
+            return data[D];
+        }
+
+        //2차원 배열을 array[][0]을 기준으로 정렬
+        static int[][] twoDimensionalArraySort(int standardIndex, int n, int[][] array)
+        {
+            for( int i=0; i<n ; i++ )
+            {
+                for(int j=0; j<n-1-i; j++)
+                {
+                    if( array[i][standardIndex] > array[j][standardIndex] )
+                    {
+                        int[] tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+
+}
